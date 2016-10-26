1dc3c8d4918d99f739998c332d17801a3187b0ac
Aravind <aravindgg55@gmail.com>
2016-10-26 23:49:57 +0530

    initial commit

---
+++ src/inventory/management/Inventory.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventory.management;
+
+import java.util.*;
+
+/**
+ *
+ * @author aravind
+ */
+public class Inventory {
+    private String productName;
+    int quantity;
+    Date d;
+    public String getProductName()
+    {
+        return productName;
+    }
+    
+    public int getQuantity()
+    {
+        return quantity;
+    }
+    
+    public void setProductName()
+    {
+        
+    }
+}
